80ee76bf43324843113d93cadbb54ca9bd2929e8
Firman Diatullah Guna Darma <darmaroyale@gmail.com>
2022-11-25 13:08:40 +0700

    Update server.js
    init hapi framework

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log('hallo testing');
+const Hapi = require('@hapi/hapi')
+
+const init = async () => {
+    const server = Hapi.Server({
+        port: 5100,
+        host: 'localhost',
+    })
+
+    await server.start()
+    console.log(`server berjalan pada ${server.info.uri}`)
+}
+
+
+
+init()
