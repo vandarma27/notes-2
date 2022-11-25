14b6cd6f8e68ddc5b67b243082458a7dfec3ab5e
Firman Diatullah Guna Darma <darmaroyale@gmail.com>
2022-11-25 13:38:55 +0700

    Update routes.js
    menambahkan routes get notes

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,12 @@ const routes = [
         method: 'POST',
         path: '/notes',
         handler: addNoteHandler
+    },
+
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: () => {},
     }
 ]
 
